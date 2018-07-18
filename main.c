154789bdbe866d3e91f5285e327cb1878a711728
NikitaBolsunovskiy <41104234+NikitaBolsunovskiy@users.noreply.github.com>
2018-07-18 18:02:40 +0300

    Update

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 #define SizeX 5
 #define SizeY 7
 
 void solution1();
 
+void solution2();
 
 int main() {
 
-    solution1();
+    //solution1();
+    solution2();
 
     return 0;
 }
@@ -18,6 +21,29 @@ void print(int n, int m, int a[SizeX][SizeY]){
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             printf("%4d", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void printMatrix(int n, int m, int * a,char *str1, char*str2){
+    int i,j;
+
+    printf("        ");
+    for (int j = 1; j < m; ++j) {
+        printf("%4c", str2[j-1]);
+    }
+    printf("\n");
+
+
+    for (int i = 0; i < n; ++i) {
+        if(i>0){
+            printf("%4c", str1[i-1]);
+        } else {
+            printf("    ");
+        }
+        for (int j = 0; j < m; ++j) {
+            printf("%4d", a[i*m+j]);
         }
         printf("\n");
     }
@@ -79,4 +105,58 @@ void solution1(){
 
     printf("Карта количества возможных путей:\n");
     print(SizeX,SizeY,a);
+}
+
+int max(int a, int b){
+    return (a>=b)?a:b;
+}
+
+int lcs_length(char *a,char *b){
+
+    int n = strlen(a) + 1;
+    int m = strlen(b) + 1;
+
+    printf("Первая строка: %s\n",a);
+    printf("Длина: %d\n",n);
+    printf("Вторая строка: %s\n",b);
+    printf("Длина: %d\n",m);
+
+    int matrix[n][m];
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            matrix[i][j] = 0;
+        }
+    }
+
+    printMatrix(n,m,matrix,a,b);
+
+    for (int i = 1; i < n; ++i) {
+        for (int j = 1; j < m; ++j) {
+            if(a[i-1]=='\0'||b[j-1]=='\0') {
+                matrix[i][j] = 0;
+            } else {
+                if (a[i - 1] == b[j - 1]) {
+                    matrix[i][j] = 1 + matrix[i - 1][j - 1];
+                } else {
+                    matrix[i][j] = max(matrix[i][j - 1], matrix[i - 1][j]);
+                }
+            }
+        }
+    }
+
+    //puts("Рассчитанная карта чисел:");
+    //print(n,m,matrix);
+
+}
+
+void solution2(){
+
+    //2. Решить задачу о нахождении длины максимальной последовательности с помощью матрицы.
+
+    int lcs_l = 0;
+
+    lcs_l = lcs_length("geekminds","geekbrains");
+
+
 }
