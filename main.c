a57d7c10ae592bf33005dfab9667a1181d335d90
NikitaBolsunovskiy <41104234+NikitaBolsunovskiy@users.noreply.github.com>
2018-07-19 11:24:58 +0300

    Update

--- main.c
+++ main.c
@@ -3,15 +3,20 @@
 
 #define SizeX 5
 #define SizeY 7
+#define ChessBoardSizeX 8
+#define ChessBoardSizeY 8
+
+int board[ChessBoardSizeX][ChessBoardSizeY];
 
 void solution1();
-
 void solution2();
+void solution3();
 
 int main() {
 
     //solution1();
-    solution2();
+    //solution2();
+    solution3();
 
     return 0;
 }
@@ -156,5 +161,167 @@ void solution2(){
 
     printf("Длина наибольшей общей последовательности: %d\n",lcs_l);
 
+
+}
+
+void Zero(int n, int m, int a[ChessBoardSizeX][ChessBoardSizeY]){
+    int i, j;
+    for (i=0; i<n; i++)
+        for (j=0; j<m; j++)
+            a[i][j] = 0;
+}
+
+void printBoard(int n, int m, int a[ChessBoardSizeX][ChessBoardSizeY]){
+    int i,j;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            printf("%4d", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int CheckBoard() {
+
+}
+
+int movePossible(int x, int y){
+
+    // Если выходим за границы поля...
+    if (x<0 || x >= ChessBoardSizeX || y < 0 || y >= ChessBoardSizeY){
+        return 0;
+    }
+    //Если по клетке не ходили...
+    if (board[x][y] == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int countFreeSpasesForMove(int x, int y){
+
+    int counter = 0;
+
+    if (x<0 || x >= ChessBoardSizeX || y < 0 || y >= ChessBoardSizeY){
+        return 1000;
+    }
+
+    if(movePossible(x-2,y-1)) counter++;
+    if(movePossible(x-2,y+1)) counter++;
+    if(movePossible(x-1,y+2)) counter++;
+    if(movePossible(x+1,y+2)) counter++;
+    if(movePossible(x+2,y-1)) counter++;
+    if(movePossible(x+2,y+1)) counter++;
+    if(movePossible(x-1,y-2)) counter++;
+    if(movePossible(x+1,y-2)) counter++;
+    return counter;
+}
+
+void analyzePossibleMoves(int x, int y, int possMoves[8][3]){
+    possMoves[0][0] = x-2;
+    possMoves[0][1] = y-1;
+    possMoves[0][2] = countFreeSpasesForMove(x-2,y-1);
+    possMoves[1][0] = x-2;
+    possMoves[1][1] = y+1;
+    possMoves[1][2] = countFreeSpasesForMove(x-2,y+1);
+    possMoves[2][0] = x-1;
+    possMoves[2][1] = y+2;
+    possMoves[2][2] = countFreeSpasesForMove(x-1,y+2);
+    possMoves[3][0] = x+1;
+    possMoves[3][1] = y+2;
+    possMoves[3][2] = countFreeSpasesForMove(x+1,y+2);
+    possMoves[4][0] = x+2;
+    possMoves[4][1] = y-1;
+    possMoves[4][2] = countFreeSpasesForMove(x+2,y-1);
+    possMoves[5][0] = x+2;
+    possMoves[5][1] = y+1;
+    possMoves[5][2] = countFreeSpasesForMove(x+2,y+1);
+    possMoves[6][0] = x-1;
+    possMoves[6][1] = y-2;
+    possMoves[6][2] = countFreeSpasesForMove(x-1,y-2);
+    possMoves[7][0] = x+1;
+    possMoves[7][1] = y-2;
+    possMoves[7][2] = countFreeSpasesForMove(x+1,y-2);
+}
+
+void sortPossibleMoves(int possMoves[8][3]){
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 7; ++j) {
+            if(possMoves[j][2]>possMoves[j+1][2]){
+                int temp[3] = {possMoves[j][0],possMoves[j][1],possMoves[j][2]};
+                possMoves[j][0] = possMoves[j+1][0];
+                possMoves[j][1] = possMoves[j+1][1];
+                possMoves[j][2] = possMoves[j+1][2];
+                possMoves[j+1][0] = temp[0];
+                possMoves[j+1][1] = temp[1];
+                possMoves[j+1][2] = temp[2];
+            }
+        }
+    }
+}
+
+int SearchSolution(int n,int x, int y){
+
+    // Если ход следующий после количества клеток, то все удалось...
+    if (n== ChessBoardSizeY*ChessBoardSizeX + 1) return 1;
+
+    int result = 0;
+
+    // Массив, в который положим возможные координаты и количество возможных ходов для них...
+    int possibleMoves[8][3];
+    analyzePossibleMoves(x,y,possibleMoves);
+    sortPossibleMoves(possibleMoves);
+
+    for (int i = 0; i < 8; ++i) {
+        if(movePossible(possibleMoves[i][0],possibleMoves[i][1])){
+            board[possibleMoves[i][0]][possibleMoves[i][1]] = n;
+            if(SearchSolution(n+1,possibleMoves[i][0],possibleMoves[i][1])){
+                return 1;
+            }
+            board[possibleMoves[i][0]][possibleMoves[i][1]] = 0;
+        }
+    }
+
+    return 0;
+}
+
+void solution3(){
+
+//    3. ***Требуется обойти конём шахматную доску размером NxM, пройдя через все поля доски по
+//    одному разу. Здесь алгоритм решения такой же как и в задаче о 8 ферзях. Разница только в проверке
+//    положения коня.
+//    Для коня суть в том, что на каждом шаге конь может сходить в строго оределенные клетки, получается на кажой итерации SearchSolution нужно передавать где сейчас конь и может ли он куда-то сходить...
+//    Будем считать, что если в ячейке Board стоит 0 - то конь там еще не был, если не ноль, то был. Записывать в ячейку будем номер текущего хода, чтобы потом наглядно проверить...
+
+
+    //    Результаты тестирования: на поле 5 на 5 быстро нашелся вариант.
+    //      на поле 6 на 6 быстро нашелся вариант.
+    // на поле 7 на 7 не дождался реузльтата, почитав про правило Варнсдорфа решил попробовать применить его.
+    // https://forany.xyz/a-16?pg=8
+    // применил правило Варнсдорфа и заработало...
+
+    Zero(ChessBoardSizeX,ChessBoardSizeY,board);
+    printBoard(ChessBoardSizeX,ChessBoardSizeY,board);
+    int i,j;
+    int result = 0;
+    for (int i = 0; i < ChessBoardSizeX; i++) {
+        for (int j = 0; j < ChessBoardSizeY; j++) {
+            board[i][j] = 1;
+            if (SearchSolution(2,i,j)) {
+                result = 1;
+                break;
+            } else {
+                printf("\n\n");
+                printBoard(ChessBoardSizeX,ChessBoardSizeY,board);
+                board[i][j] = 0;
+            }
+        }
+        if (result == 1){
+            break;
+        }
+    }
+
+    printf("\n\n");
+    printBoard(ChessBoardSizeX,ChessBoardSizeY,board);
 
 }
